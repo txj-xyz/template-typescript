a0af68f322a13e05b2fd81c09b070f607810b9dc
txj-xyz <7461476+txj-xyz@users.noreply.github.com>
2023-12-20 17:05:50 -0700

    chr: update base package to include more scripts

--- src/index.ts
+++ src/index.ts
@@ -2,27 +2,25 @@
  * Logger to parse messages or stack traces
  */
 class Logger {
-    
-    /**
-     * Incoming state message from construction.
-     * Typically a message string or object.
-     */
-    public payload: any[];
+  /**
+   * Incoming state message from construction.
+   * Typically a message string or object.
+   */
+  public payload: any[];
 
-    constructor(...data: any[]) {
-        this.payload = data;
-    }
+  constructor(...data: any[]) {
+    this.payload = data;
+  }
 
-    /**
-     * Send a message to the console.
-     * @param data Any incoming message to log to the console.
-     * @returns `void`
-     */
-    public static log(...data: any[]): void {
-        return void console.log(...data);
-    }
+  /**
+   * Send a message to the console.
+   * @param data Any incoming message to log to the console.
+   * @returns `void`
+   */
+  public static log(...data: any[]): void {
+    return void console.log(...data);
+  }
 }
-
 
 // Log out a message
 Logger.log("This is an example message");
